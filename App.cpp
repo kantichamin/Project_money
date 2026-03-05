db02334ffeeda19120d7f8511676344a62b9bf68
Meowwwwwwwwwwwwwwww <97337270+Meowwwwwwwwwwwwwwww@users.noreply.github.com>
2026-03-05 17:00:48 +0700

    App.cpp

--- App.cpp
+++ App.cpp
@@ -4,7 +4,10 @@
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit() {
-    MainFrame* frame = new MainFrame("Rainy Day Dashboard");
-    frame->Show(true);
+    MainFrame* frame = new MainFrame("Finance");
+    frame->SetClienSize(800, 750);
+    frame->CenterOnScreen();
+    frame->Show();
     return true;
+
 }
