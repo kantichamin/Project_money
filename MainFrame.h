0e1e790011a801082cad3851180d66e6edfe86fe
jirattikan-pk <mindmook.2006@gmail.com>
2026-03-07 20:17:14 +0700

    Add files via upload

---
+++ MainFrame.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <wx/wx.h>
+#include <wx/listctrl.h>
+#include "InandEx.h"
+
+class MainFrame : public wxFrame
+{
+public:
+    MainFrame(const wxString& title);
+
+private:
+    wxListCtrl* m_listCtrl;
+    InandEx* inandexPanel;
+};
